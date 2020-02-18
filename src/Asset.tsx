ff0b897e3c6f767562178d14d83c820c316e4aef
Ayush Kumar <kmar.ayush@gmail.com>
2020-02-18 20:23:14 +1100

    Update layout of the assets

--- src/Asset.tsx
+++ src/Asset.tsx
@@ -14,16 +14,20 @@ type AssetProps = {
 
 export const Asset= ({name, lastBoughtPrice, currentPrice}:AssetProps) => {
 return(
-    <Row>
-        <Col span={5}>
-        <Text strong>Asset Type : {name}</Text> 
-        </Col>
-        <Col span={5}>
-            <Text strong>Last bought price: ${lastBoughtPrice}</Text> 
-        </Col>
-        <Col span={5}>
-            <Text strong>Current price: ${currentPrice}</Text> 
-        </Col>
-    </Row>
+    <div>
+        <Row>
+            <Col span={5}>
+            <Text strong>Asset Type : {name}</Text> 
+            </Col>
+            <Col span={5}>
+                <Text strong>Last bought price: ${lastBoughtPrice}</Text> 
+            </Col>
+        </Row>
+        <Row>
+        <Col offset={5} span={5}>
+                <Text strong>Current price: ${currentPrice}</Text> 
+            </Col>
+        </Row>
+    </div>
     )
 }
